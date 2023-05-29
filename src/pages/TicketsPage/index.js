1a10dbfbd6b0ce94c153a03a76ef5e36f9f21b07
anarehder <anapaula.rehder@gmail.com>
2023-05-29 08:00:04 -0300

    Feat: Passagens Lista

--- src/pages/TicketsPage/index.js
+++ src/pages/TicketsPage/index.js
@@ -4,8 +4,9 @@ import { useNavigate, useParams } from "react-router-dom";
 import { useEffect, useContext, useState } from "react";
 import { CityContext } from "../../contexts/CityContext.js";
 import gif from "../../assets/images/loading-2.gif";
-import voo from "../../assets/images/aviao1.png"
+import voo from "../../assets/images/aviao1.png";
 import axios from "axios";
+import dayjs from "dayjs";
 
 export default function HomePage() {
     const { cityID } = useParams();
@@ -13,17 +14,27 @@ export default function HomePage() {
     const [listaPassagens, setListaPassagens] = useState([]);
     const [carregando, setCarregando] = useState(false);
     const [cityName, setCityName] = useState("");
+    const day = dayjs();
 
     const [minValue, setMinValue] = useState("");
     const [maxValue, setMaxValue] = useState("");
+
+    //const teste = listaPassagens[0].depDate1;
+    //const teste2 = dayjs(teste.format('DD-MM-YYYY HH:mm'));
+    //console.log(teste2);
 
     useEffect(() => {
         setCarregando(true);
         const promise = axios.get(`${process.env.REACT_APP_API_URL}/tickets/city/${cityID}`);
         promise.then(resposta => {
             setListaPassagens(resposta.data);
+            setCityName(resposta.data[0].destiny);
             setCarregando(false);
-            console.log(listaPassagens)
+            const teste = resposta.data[0].depDate1;
+            const data = new Date(teste);
+            const data2 = `${new Date(teste).getDate()}/${new Date(teste).getMonth()}/${new Date(teste).getFullYear()}`;
+            const hour = `${data.getHours()}:${data.getMinutes()}`;
+            console.log(data2, hour)
         })
         promise.catch(erro => {
             alert(erro.response.data);
@@ -36,7 +47,7 @@ export default function HomePage() {
             <OutboxLogo />
             {carregando === true ? <GifContainer> <img src={gif} alt="gif" /></GifContainer> :
                 <>
-                    <h1> Passagens para {city}</h1>
+                    <h1> Passagens para {cityName}</h1>
                     <TicketsAll>
                         <Filter> MENU
                             <form action="/action_page.php">
@@ -51,9 +62,11 @@ export default function HomePage() {
                             {listaPassagens.map((item) =>
                                 <OneTicket>
                                     <img src={voo} alt="aviao" key={item.id} />
-                                    <h2> Data e Hora</h2>
-                                    <h2> Preco </h2>
-                                    <h2 >Local Partida</h2>
+                                    <h2> Data Partida: {new Date(item.depDate1).getDate()}/{new Date(item.depDate1).getMonth()}/{new Date(item.depDate1).getFullYear()}</h2>
+                                    <h2> Hora Partida: {new Date(item.depDate1).getUTCHours()}h {new Date(item.depDate1).getMinutes()} min</h2>
+                                    <h2> Preço: R$ {item.price.replace(".",",")}</h2>
+                                    <h2 >Local Partida: {item.origin}</h2>
+                                    <h2 >Classe: {item.class}</h2>
                                 </OneTicket>
                             )}
                         </Tickets>
